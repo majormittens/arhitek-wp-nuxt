388a47ecf09c6e763bdef1f2886a43ed60fdb5fe
bws <bws@C02YD1P6JG5H.grubhub.local>
2022-12-04 19:51:34 +0200

    fix

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,12 +1,13 @@
 <template>
 <section class="w-full max-w-2xl px-6 py-4 mx-auto bg-white dark:bg-gray-800">
     <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
-        <a href="#" class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
+        <a href="#" class="flex flex-col items-center align-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
             <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
             </svg>
 
-            <span class="mt-2">Piața C.A. Rosetti nr. 3, ap. 12</span>
+            <span class="mt-2">Piața C.A. Rosetti</span>
+            <span>nr. 3, ap. 12</span>
         </a>
 
         <a href="#" class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-200 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
